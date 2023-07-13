db6303b9b67e0ee84449cf12c1f90cd343181ebf
Pratik Kamle <pratik.kamle@rapidcircle.com>
2023-07-13 17:40:09 +0530

    Modifications to the existing GitHub action

--- index.js
+++ index.js
@@ -1,29 +1,77 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
+const fs = require('fs');
+const artifact = require('@actions/artifact');
+
+// Function to fetch the overall workflow status using the GitHub API
+async function getWorkflowStatus(runId) {
+  const response = await axios.get(`https://api.github.com/repos/pratikkamle/http-client-custom-action/actions/runs/${runId}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+      Accept: 'application/vnd.github.v3+json'
+    }
+  });
+
+  return response.data.conclusion;
+}
 
 async function run() {
   try {
     // Get inputs using the GitHub Core library
-    const data = core.getInput('data');
-    const url = core.getInput('url');
-    const headers = core.getInput('customHeaders');
-    const method = core.getInput('method');
+    const Output = core.getInput('Output');
+    const LeapDNABaseUrl = core.getInput('LeapDNABaseUrl');
+    const clientId = core.getInput('ClientId');
+    const clientSecret = core.getInput('ClientSecret');
+    const tenantId = core.getInput('TenantId');
+    const certificateBase64 = core.getInput('CertificateBase64');
+    // const WorkflowStatus = core.getInput('WorkflowStatus');
+    const method = "post";
+
+    // const headers = core.getInput('headers');
 
     // Parse the headers input as JSON
-    const parsedHeaders = JSON.parse(headers);
+    // const parsedHeaders = JSON.parse(headers);
+
+    // Fetch values from environment variables if inputs are not provided
+    const ClientId = clientId || process.env.CLIENT_ID;
+    const ClientSecret = clientSecret || process.env.CLIENT_SECRET;
+    const TenantId = tenantId || process.env.TENANT_ID;
+    const CertificateBase64 = certificateBase64 || process.env.CERTIFICATE_BASE_64;
+    console.log('ClientId:', ClientId);
+    console.log('ClientSecret:', ClientSecret);
+    console.log('TenantId:', TenantId);
+    console.log('CertificateBase64:', CertificateBase64);
+    // Get the current job status
+    const jobStatus = process.env.GITHUB_JOB;
+
+    // Get the current workflow run ID
+    const runId = process.env.GITHUB_RUN_ID;
+
+    // Fetch the overall workflow status using the GitHub API
+    const workflowStatus = await getWorkflowStatus(runId);
+
+    // Use the workflow status in your script logic
+    console.log('Current Job Status:', jobStatus);
+    console.log('Current Workflow Run ID:', runId);
+    console.log('Current Workflow Status:', workflowStatus);
+
+    const parsedHeaders = {
+      "Content-Type": "application/json"
+    };
 
     // Create the request body
-    const requestBody = {
-      data: data
-    };
+    const parsedOutput = JSON.parse(Output);
+    // const requestBody = {
+    //   Output: Output
+    // };
 
     // Set up the axios request configuration
     const axiosConfig = {
       method: method.toUpperCase(),
-      url: url,
+      url: LeapDNABaseUrl,
       headers: parsedHeaders,
-      data: requestBody
+      data: parsedOutput
     };
 
     // Send the HTTP request
@@ -35,6 +83,17 @@ async function run() {
     // Set the output using GitHub library to be accessed by the workflows
     core.setOutput('response', response.data);
     core.setOutput('headers', response.headers);
+
+    // Write the JSON output to a file
+    const outputFile = 'output.json';
+    fs.writeFileSync(outputFile, JSON.stringify(parsedOutput));
+
+    // Publish the output file as an artifact
+    const artifactClient = artifact.create();
+    const artifactName = 'my-artifact';
+    const files = [outputFile];
+    const rootDirectory = '.';
+    await artifactClient.uploadArtifact(artifactName, files, rootDirectory);
   } catch (error) {
     // Handle any errors that occur during the execution
     console.error('Error:', error.message);
